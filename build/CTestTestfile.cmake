af7390d8fd133c682bc533d6b96759f48af58c69
JanLukasGithub <jan@garloff.de>
2022-02-06 13:59:58 +0100

    Finally succesfully removed build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jan/visual-studio-code-workspace/OpenGL
-# Build directory: /home/jan/visual-studio-code-workspace/OpenGL/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
