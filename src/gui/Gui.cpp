bbce0519789a42b0355ba2bf8f0ffe00037b77db
JanLukasGithub <jan@garloff.de>
2022-02-21 22:38:06 +0100

    [Gui]: Use triangle strip to render rectangle
    Using two seperate traingles is a bit less efficient

--- src/gui/Gui.cpp
+++ src/gui/Gui.cpp
@@ -22,8 +22,6 @@ void Gui::drawRectangle(float32 x, float32 y, float32 w, float32 h, int32 color)
         x, y,
         x + w, y,
         x, y + h,
-        x, y + h,
-        x + w, y,
         x + w, y + h
     };
 
@@ -36,12 +34,12 @@ void Gui::drawRectangle(float32 x, float32 y, float32 w, float32 h, int32 color)
     glGenBuffers(1, &vertexBufferId);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBufferId);
     // Copy data to buffer
-    glBufferData(GL_ARRAY_BUFFER, sizeof(buffer[0]) * 12, buffer, GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(buffer[0]) * 8, buffer, GL_DYNAMIC_DRAW);
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
 
     // Draw
-    glDrawArrays(GL_TRIANGLES, 0, 6);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
     // Delete buffer and array
     glDeleteBuffers(1, &vertexBufferId);
