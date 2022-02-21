2898cbf9cfd10d2e6b63ba68bcc763295b271538
JanLukasGithub <jan@garloff.de>
2022-02-21 18:11:33 +0100

    [GuiScreen]: Properly use newlines

--- src/gui/GuiScreen.cpp
+++ src/gui/GuiScreen.cpp
@@ -14,10 +14,13 @@
 GuiScreen::GuiScreen(SDL_Window* window) : Gui() {
 	SDL_GetWindowSize(window, &m_width, &m_height);
 }
+
 GuiScreen::GuiScreen(SDL_Window* window, float zLevel) : Gui(zLevel) {
 	SDL_GetWindowSize(window, &m_width, &m_height);
 }
 #pragma GCC diagnostic pop
+
 GuiScreen::GuiScreen(int height, int width) : Gui(), m_width{ width }, m_height{ height } {}
+
 GuiScreen::GuiScreen(float zLevel, int height, int width) : Gui(zLevel), m_width{ width }, m_height{ height } {}
 
