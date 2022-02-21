d000f567cf5b3e05eede7efe734cceee7b3b5683
JanLukasGithub <jan@garloff.de>
2022-02-21 20:41:04 +0100

    [2dShader]: Support only using alpha or using all colors

--- src/shaders/2d.fs
+++ src/shaders/2d.fs
@@ -3,7 +3,11 @@
 in vec2 v_texture_coords;
 
 uniform sampler2D u_texture;
+uniform bool u_onlyAlpha;
 
 void main() {
-    gl_FragColor = vec4(texture2D(u_texture, v_texture_coords).a);
+    if (u_onlyAlpha)
+        gl_FragColor = vec4(texture2D(u_texture, v_texture_coords).a);
+    else
+        gl_FragColor = vec4(texture2D(u_texture, v_texture_coords));
 }
