5db0952ab009ac5c99e091b58be6488b82fe8774
JanLukasGithub <jan@garloff.de>
2022-03-25 20:36:43 +0100

    ModelFile: Implement addModelFile()

--- src/models/modelFile.cpp
+++ src/models/modelFile.cpp
@@ -1,5 +1,14 @@
 #include "modelFile.h"
 
+int32 ModelFile::addModelFile(const char* filename) {
+    int32 index = indexOf(filename);
+    if (index != -1)
+        return index;
+
+    modelFiles.push_back(ModelFile());
+    return modelFiles.size() - 1;
+}
+
 int32 ModelFile::indexOf(const char* filename) {
     return std::find(modelFiles.begin(), modelFiles.end(), filename).base() - modelFiles.data();
 }
