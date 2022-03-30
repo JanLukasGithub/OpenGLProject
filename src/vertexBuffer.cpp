d6cd756b1b9f5fc7e64a201fbb35e209aea90def
JanLukasGithub <jan@garloff.de>
2022-03-30 19:32:50 +0200

    Add a copy constrcutor to VertexBuffer

--- src/vertexBuffer.cpp
+++ src/vertexBuffer.cpp
@@ -1,12 +1,35 @@
 #include "vertexBuffer.h"
 
-VertexBuffer::VertexBuffer(const void* data, const uint64 numVertices) noexcept {
+VertexBuffer::VertexBuffer(const void* data, const uint64 numVertices) noexcept : m_size{ sizeof(Vertex) * numVertices } {
 	glGenVertexArrays(1, &m_vao);
 	glBindVertexArray(m_vao);
 
 	glGenBuffers(1, &m_bufferId);
 	glBindBuffer(GL_ARRAY_BUFFER, m_bufferId);
-	glBufferData(GL_ARRAY_BUFFER, numVertices * sizeof(Vertex), data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, m_size, data, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, normal));
+
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, tangent));
+
+	glEnableVertexAttribArray(3);
+	glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, textureCoords));
+
+	glBindVertexArray(0);
+}
+
+VertexBuffer::VertexBuffer(const VertexBuffer& vbo) noexcept {
+	glGenVertexArrays(1, &m_vao);
+	glBindVertexArray(m_vao);
+
+	glGenBuffers(1, &m_bufferId);
+	glBindBuffer(GL_ARRAY_BUFFER, m_bufferId);
+	glCopyBufferSubData(vbo.m_bufferId, m_bufferId, 0, 0, m_size);
 
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
@@ -25,6 +48,7 @@ VertexBuffer::VertexBuffer(const void* data, const uint64 numVertices) noexcept 
 
 VertexBuffer::~VertexBuffer() noexcept {
 	glDeleteBuffers(1, &m_bufferId);
+	glDeleteVertexArrays(1, &m_vao);
 }
 
 VertexBuffer* VertexBuffer::bind() noexcept {
--- src/vertexBuffer.h
+++ src/vertexBuffer.h
@@ -9,9 +9,11 @@ class VertexBuffer {
 private:
 	GLuint m_bufferId{ 0 };
 	GLuint m_vao{ 0 };
+	GLsizeiptr m_size{ 0 };
 
 public:
 	VertexBuffer(const void* data, const uint64 numVertices) noexcept;
+	VertexBuffer(const VertexBuffer& vbo) noexcept;
 	virtual ~VertexBuffer() noexcept;
 
 	VertexBuffer* bind() noexcept;
