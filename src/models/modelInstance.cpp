5f974fac5c3457e679443a1b7d5fe1ef0e9811ff
JanLukasGithub <jan@garloff.de>
2022-04-01 13:13:05 +0200

    Add const qualifiers where possible

--- src/models/modelInstance.cpp
+++ src/models/modelInstance.cpp
@@ -1,29 +1,29 @@
 #include "modelInstance.h"
 
-void ModelInstance::initUniforms(Shader* shader) {
+void ModelInstance::initUniforms(const Shader* shader) {
 	ModelInstance::modelMatLocation = glGetUniformLocation(shader->getShaderId(), "u_modelMat");
 }
 
 ModelInstance::ModelInstance() {}
 
-ModelInstance::ModelInstance(glm::vec3 position) : m_modelMat{ glm::translate(glm::mat4(1.0f), position) } {}
+ModelInstance::ModelInstance(const glm::vec3 position) : m_modelMat{ glm::translate(glm::mat4(1.0f), position) } {}
 
-ModelInstance::ModelInstance(glm::mat4 modelMat) : m_modelMat{ modelMat } {}
+ModelInstance::ModelInstance(const glm::mat4 modelMat) : m_modelMat{ modelMat } {}
 
-void ModelInstance::translate(glm::vec3 translation) {
+void ModelInstance::translate(const glm::vec3 translation) {
 	m_modelMat = glm::translate(m_modelMat, translation);
 }
 
-void ModelInstance::rotate(glm::vec3 rotation) {
+void ModelInstance::rotate(const glm::vec3 rotation) {
 	m_modelMat = glm::rotate(m_modelMat, rotation.x, glm::vec3(1, 0, 0));
 	m_modelMat = glm::rotate(m_modelMat, rotation.y, glm::vec3(0, 1, 0));
 	m_modelMat = glm::rotate(m_modelMat, rotation.z, glm::vec3(0, 0, 1));
 }
 
-void ModelInstance::scale(glm::vec3 scale) {
+void ModelInstance::scale(const glm::vec3 scale) {
 	m_modelMat = glm::scale(m_modelMat, scale);
 }
 
-glm::mat4& ModelInstance::getModelMat() noexcept {
+const glm::mat4& ModelInstance::getModelMat() noexcept {
 	return m_modelMat;
 }
--- src/models/modelInstance.h
+++ src/models/modelInstance.h
@@ -12,7 +12,7 @@ class ModelInstance {
 public:
 	// Location of model mat uniform
 	inline static int modelMatLocation;
-	static void initUniforms(Shader* shader);
+	static void initUniforms(const Shader* shader);
 
 private:
 	// Stores translation, rotation and scale
@@ -22,20 +22,20 @@ public:
 	// Model at position 0, 0, 0
 	ModelInstance();
 	// Model at specified position
-	ModelInstance(glm::vec3 position);
+	ModelInstance(const glm::vec3 position);
 	// Model using the specified modelMat
-	ModelInstance(glm::mat4 modelMat);
+	ModelInstance(const glm::mat4 modelMat);
 	// Deletes meshes
 	virtual ~ModelInstance() {};
 
 	// Translate the model
-	void translate(glm::vec3 translation);
+	void translate(const glm::vec3 translation);
 	// Rotate the model, with rotation in radians
-	void rotate(glm::vec3 rotation);
+	void rotate(const glm::vec3 rotation);
 	// Scale the model
-	void scale(glm::vec3 scale);
+	void scale(const glm::vec3 scale);
 
-	glm::mat4& getModelMat() noexcept;
+	const glm::mat4& getModelMat() noexcept;
 };
 
 #endif /* MODELS_MODEL_H_ */
