b91e4825dad123cc272b7b53fc3acb8dc09dffd8
JanLukasGithub <jan@garloff.de>
2022-04-02 11:12:38 +0200

    Remove if statement in 2d.fs
    Doesn't improve performance because 2d.fs is unused rn

--- src/shaders/2d.fs
+++ src/shaders/2d.fs
@@ -7,8 +7,5 @@ uniform bool u_hasTexture;
 uniform vec4 u_color;
 
 void main() {
-    if (u_hasTexture)
-        gl_FragColor = vec4(texture2D(u_texture, v_textureCoords));
-    else
-        gl_FragColor = u_color;
+    gl_FragColor = int(u_hasTexture) * vec4(texture2D(u_texture, v_textureCoords)) + int(!u_hasTexture) * u_color;
 }
