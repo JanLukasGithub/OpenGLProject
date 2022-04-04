a2d1f6a58a9cea6fa427d0992de6a91f11bc5617
JanLukasGithub <jan@garloff.de>
2022-04-04 20:31:15 +0200

    Add vertex and index buffer members
    Also change type of height map array to be only 16bit per value instead of 32bit

--- src/terrain/terrain.h
+++ src/terrain/terrain.h
@@ -1,6 +1,8 @@
 #ifndef TERRAIN_H_
 #define TERRAIN_H_
 
+#include "../../lib/glm/detail/type_half.hpp"
+
 #include "../renderer/IRenderable.h"
 #include "../utils.h"
 #include "../shader.h"
@@ -13,14 +15,21 @@ private:
 public:
     static void initUniforms(const Shader* shader);
 private:
+    // Vertex Buffer 
+    int m_vao, m_vboBufferId;
+    // Index Buffer
+    int m_iboBufferId;
+
     // Offset of the smallest corner from 0, 0, 0
     const int m_offsetX, m_offsetZ;
 
     // Width and length of the terrain chunk this represents
     const int m_sizeX, m_sizeZ;
 
-    // Terrain heights of the terrain chunk this represents. Has the size m_sizeX * m_sizeZ
-    const float32 m_heightMap[];
+    // Terrain heights of the terrain chunk this represents. Has the size (m_sizeX / 2) * (m_sizeZ / 2) = (m_sizeX * m_sizeZ) / 4
+    // (16384m / 2m)² * 2Byte ≈ 268MB. 32km ≈ 1GB
+    // short just to get a 16bit value, treat as a float
+    const short m_heightMap[];
 
 public:
     // Renders this part of the terrain
