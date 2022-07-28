f196103b84075b9fac3305ef11f56537cf18f979
JanLukasGithub <jan@garloff.de>
2022-07-28 20:10:49 +0200

    Add copy constructor and assignment operator to IB

--- src/buffer/indexBuffer.cpp
+++ src/buffer/indexBuffer.cpp
@@ -14,8 +14,41 @@ IndexBuffer::IndexBuffer(const IndexBuffer& ibo) noexcept : m_size{ ibo.m_size }
 	glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_ELEMENT_ARRAY_BUFFER, 0, 0, m_size);
 }
 
+IndexBuffer::IndexBuffer(IndexBuffer&& ibo) noexcept : m_size{ ibo.m_size }, m_bufferId{ ibo.m_bufferId } {
+	ibo.m_bufferId = 0;
+}
+
 IndexBuffer::~IndexBuffer() noexcept {
 	glDeleteBuffers(1, &m_bufferId);
+}
+
+IndexBuffer& IndexBuffer::operator=(const IndexBuffer& ibo) {
+	if (&ibo == this)
+		return *this;
+
+	glDeleteBuffers(1, &m_bufferId);
+
+	glBindBuffer(GL_COPY_READ_BUFFER, ibo.m_bufferId);
+
+	glGenBuffers(1, &m_bufferId);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_bufferId);
+	glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_ELEMENT_ARRAY_BUFFER, 0, 0, m_size);
+
+	return *this;
+}
+
+IndexBuffer& IndexBuffer::operator=(IndexBuffer&& ibo) {
+	if (&ibo == this)
+		return *this;
+
+	glDeleteBuffers(1, &m_bufferId);
+
+	m_size = ibo.m_size;
+	m_bufferId = ibo.m_bufferId;
+
+	ibo.m_bufferId = 0;
+
+	return *this;
 }
 
 IndexBuffer* IndexBuffer::bind() noexcept {
--- src/buffer/indexBuffer.h
+++ src/buffer/indexBuffer.h
@@ -13,7 +13,11 @@ private:
 public:
 	IndexBuffer(const void* data, const uint64 numIndices, const uint8 elementSize) noexcept;
 	IndexBuffer(const IndexBuffer& ibo) noexcept;
+	IndexBuffer(IndexBuffer&& ibo) noexcept;
 	virtual ~IndexBuffer() noexcept;
+
+	IndexBuffer& operator=(const IndexBuffer& ibo);
+	IndexBuffer& operator=(IndexBuffer&& ibo);
 
 	IndexBuffer* bind() noexcept;
 	IndexBuffer* unbind() noexcept;
