8718d66f6a44cce40ef4dd6f47625e492b49c194
JanLukasGithub <jan@garloff.de>
2022-07-31 13:43:30 +0200

    Add functions to access mbo's data

--- src/buffer/modelBuffer.cpp
+++ src/buffer/modelBuffer.cpp
@@ -11,12 +11,12 @@ Model_Buffer::Model_Buffer() {}
 Model_Buffer::Model_Buffer(std::vector<glm::mat4> model_mats) : m_size(model_mats.size()) {
     glGenBuffers(1, &m_buffer_id);
     glBindBuffer(GL_ARRAY_BUFFER, m_buffer_id);
-    glBufferData(GL_ARRAY_BUFFER, m_size, model_mats.data(), GL_DYNAMIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, m_size * sizeof(glm::mat4), model_mats.data(), GL_DYNAMIC_DRAW);
 
     init_vertex_attrib_array();
 }
 
-Model_Buffer::Model_Buffer(const Model_Buffer& mbo) : m_size{ mbo.m_size } {
+Model_Buffer::Model_Buffer(const Model_Buffer& mbo) : m_size{ mbo.m_size }, m_free_space{ mbo.m_free_space } {
 	glBindBuffer(GL_COPY_READ_BUFFER, mbo.m_buffer_id);
 
     glGenBuffers(1, &m_buffer_id);
@@ -26,7 +26,7 @@ Model_Buffer::Model_Buffer(const Model_Buffer& mbo) : m_size{ mbo.m_size } {
     init_vertex_attrib_array();
 }
 
-Model_Buffer::Model_Buffer(Model_Buffer&& mbo) : m_size{ mbo.m_size }, m_buffer_id{ mbo.m_buffer_id } {
+Model_Buffer::Model_Buffer(Model_Buffer&& mbo) : m_size{ mbo.m_size }, m_buffer_id{ mbo.m_buffer_id }, m_free_space{ std::move(mbo.m_free_space) } {
     mbo.m_buffer_id = 0;
 }
 
@@ -39,6 +39,7 @@ Model_Buffer& Model_Buffer::operator=(const Model_Buffer& mbo) {
         return *this;
 
     m_size = mbo.m_size;
+    m_free_space = mbo.m_free_space;
 
     glDeleteBuffers(1, &m_buffer_id);
 
@@ -61,8 +62,59 @@ Model_Buffer& Model_Buffer::operator=(Model_Buffer&& mbo) {
 
     m_size = mbo.m_size;
     m_buffer_id = mbo.m_buffer_id;
+    m_free_space = std::move(mbo.m_free_space);
 
     mbo.m_buffer_id = 0;
 
     return *this;
+}
+
+void Model_Buffer::set(GLsizeiptr index, glm::mat4 value) {
+    glBindBuffer(GL_ARRAY_BUFFER, m_buffer_id);
+
+    glBufferSubData(GL_ARRAY_BUFFER, index * sizeof(glm::mat4), sizeof(glm::mat4), &value);
+}
+
+GLsizeiptr Model_Buffer::add(glm::mat4 value) {
+    if (m_free_space.empty()) {
+        GLuint new_buffer_id;
+
+        glBindBuffer(GL_COPY_READ_BUFFER, m_buffer_id);
+
+        glCreateBuffers(1, &new_buffer_id);
+        glBindBuffer(GL_ARRAY_BUFFER, new_buffer_id);
+        glBufferData(GL_ARRAY_BUFFER, (m_size + 1) * sizeof(glm::mat4), nullptr, GL_DYNAMIC_DRAW);
+
+        glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_ARRAY_BUFFER, 0, 0, m_size * sizeof(glm::mat4));
+        glBufferSubData(GL_ARRAY_BUFFER, m_size * sizeof(glm::mat4), sizeof(glm::mat4), &value);
+
+        glDeleteBuffers(1, &m_buffer_id);
+
+        m_buffer_id = new_buffer_id;
+        m_size++;
+
+        return m_size - 1;
+    } else {
+        GLsizeiptr add_at = m_free_space.back();
+        m_free_space.pop_back();
+
+        set(add_at, value);
+
+        return add_at;
+    }
+}
+
+void Model_Buffer::remove(GLsizeiptr index) {
+    set(index, glm::mat4(0));
+    m_free_space.push_back(index);
+}
+
+glm::mat4 Model_Buffer::get(GLsizeiptr index) {
+    glm::mat4 store_to;
+
+    glBindBuffer(GL_ARRAY_BUFFER, m_buffer_id);
+
+    glBufferSubData(GL_ARRAY_BUFFER, index * sizeof(glm::mat4), sizeof(glm::mat4), &store_to);
+
+    return store_to;
 }
--- src/buffer/modelBuffer.h
+++ src/buffer/modelBuffer.h
@@ -17,9 +17,20 @@ public:
     Model_Buffer& operator=(const Model_Buffer& mbo);
     Model_Buffer& operator=(Model_Buffer&& mbo);
 
+    /** Set the model mat at the specified index. Out of bound indices will result in an OpenGL error */
+    void set(GLsizeiptr index, glm::mat4 value);
+    /** Add the model mat to the buffer, but might have to allocate a new buffer. Returns the index of the new value */
+    GLsizeiptr add(glm::mat4 value);
+    /** Remove the model mat at the index from the buffer */
+    void remove(GLsizeiptr index);
+    /** Get the model mat at the index */
+    glm::mat4 get(GLsizeiptr index);
+
 private:
     GLuint m_buffer_id{ 0 };
     GLsizeiptr m_size{ 0 };
+
+    std::vector<GLsizeiptr> m_free_space{};
 };
 
 #endif
