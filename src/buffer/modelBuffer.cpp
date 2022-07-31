d4d9eb40ec32ef929820424e7705d21ecf1ca323
JanLukasGithub <jan@garloff.de>
2022-07-31 16:53:07 +0200

    Fix multiple issues

--- src/buffer/modelBuffer.cpp
+++ src/buffer/modelBuffer.cpp
@@ -1,6 +1,10 @@
 #include "modelBuffer.h"
 
-Model_Buffer::Model_Buffer() {}
+Model_Buffer::Model_Buffer() {
+    glGenBuffers(1, &m_buffer_id);
+    glBindBuffer(GL_ARRAY_BUFFER, m_buffer_id);
+    glBufferData(GL_ARRAY_BUFFER, 0, nullptr, GL_DYNAMIC_DRAW);
+}
 
 Model_Buffer::Model_Buffer(std::vector<glm::mat4> model_mats) : m_size(model_mats.size()) {
     glGenBuffers(1, &m_buffer_id);
@@ -9,11 +13,12 @@ Model_Buffer::Model_Buffer(std::vector<glm::mat4> model_mats) : m_size(model_mat
 }
 
 Model_Buffer::Model_Buffer(const Model_Buffer& mbo) : m_size{ mbo.m_size }, m_free_space{ mbo.m_free_space } {
-	glBindBuffer(GL_COPY_READ_BUFFER, mbo.m_buffer_id);
+    glBindBuffer(GL_COPY_READ_BUFFER, mbo.m_buffer_id);
 
     glGenBuffers(1, &m_buffer_id);
     glBindBuffer(GL_ARRAY_BUFFER, m_buffer_id);
-    glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_ARRAY_BUFFER, 0, 0, m_size);
+    glBufferData(GL_ARRAY_BUFFER, m_size * sizeof(glm::mat4), nullptr, GL_DYNAMIC_DRAW);
+    glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_ARRAY_BUFFER, 0, 0, m_size * sizeof(glm::mat4));
 }
 
 Model_Buffer::Model_Buffer(Model_Buffer&& mbo) : m_size{ mbo.m_size }, m_buffer_id{ mbo.m_buffer_id }, m_free_space{ std::move(mbo.m_free_space) } {
@@ -33,11 +38,12 @@ Model_Buffer& Model_Buffer::operator=(const Model_Buffer& mbo) {
 
     glDeleteBuffers(1, &m_buffer_id);
 
-	glBindBuffer(GL_COPY_READ_BUFFER, mbo.m_buffer_id);
+    glBindBuffer(GL_COPY_READ_BUFFER, mbo.m_buffer_id);
 
     glGenBuffers(1, &m_buffer_id);
     glBindBuffer(GL_ARRAY_BUFFER, m_buffer_id);
-    glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_ARRAY_BUFFER, 0, 0, m_size);
+    glBufferData(GL_ARRAY_BUFFER, m_size * sizeof(glm::mat4), nullptr, GL_DYNAMIC_DRAW);
+    glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_ARRAY_BUFFER, 0, 0, m_size * sizeof(glm::mat4));
 
     return *this;
 }
@@ -65,20 +71,20 @@ void Model_Buffer::set(GLsizeiptr index, glm::mat4 value) {
 
 GLsizeiptr Model_Buffer::add(glm::mat4 value) {
     if (m_free_space.empty()) {
-        GLuint new_buffer_id;
+        glBindBuffer(GL_ARRAY_BUFFER, m_buffer_id);
 
-        glBindBuffer(GL_COPY_READ_BUFFER, m_buffer_id);
-
-        glCreateBuffers(1, &new_buffer_id);
-        glBindBuffer(GL_ARRAY_BUFFER, new_buffer_id);
+        GLuint temp_buffer_id;
+        glCreateBuffers(1, &temp_buffer_id);
+        glBindBuffer(GL_COPY_READ_BUFFER, temp_buffer_id);
+        glBufferData(GL_COPY_READ_BUFFER, m_size * sizeof(glm::mat4), nullptr, GL_DYNAMIC_READ);
+        glCopyBufferSubData(GL_ARRAY_BUFFER, GL_COPY_READ_BUFFER, 0, 0, m_size * sizeof(glm::mat4));
+    
         glBufferData(GL_ARRAY_BUFFER, (m_size + 1) * sizeof(glm::mat4), nullptr, GL_DYNAMIC_DRAW);
-
         glCopyBufferSubData(GL_COPY_READ_BUFFER, GL_ARRAY_BUFFER, 0, 0, m_size * sizeof(glm::mat4));
         glBufferSubData(GL_ARRAY_BUFFER, m_size * sizeof(glm::mat4), sizeof(glm::mat4), &value);
 
-        glDeleteBuffers(1, &m_buffer_id);
+        glDeleteBuffers(1, &temp_buffer_id);
 
-        m_buffer_id = new_buffer_id;
         m_size++;
 
         return m_size - 1;
@@ -102,7 +108,7 @@ glm::mat4 Model_Buffer::get(GLsizeiptr index) {
 
     glBindBuffer(GL_ARRAY_BUFFER, m_buffer_id);
 
-    glBufferSubData(GL_ARRAY_BUFFER, index * sizeof(glm::mat4), sizeof(glm::mat4), &store_to);
+    glGetBufferSubData(GL_ARRAY_BUFFER, index * sizeof(glm::mat4), sizeof(glm::mat4), &store_to);
 
     return store_to;
 }
